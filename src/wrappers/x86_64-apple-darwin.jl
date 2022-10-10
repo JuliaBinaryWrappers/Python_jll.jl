065b08b83fda97d355de582ffc99498f6bcd4ec4
jlbuild <juliabuildbot@gmail.com>
2022-10-10 14:03:26 +0000

    Python_jll build 3.10.7+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -4,17 +4,18 @@ export libpython, python
 using Expat_jll
 using Bzip2_jll
 using Libffi_jll
+using LibMPDec_jll
 using Zlib_jll
 using XZ_jll
 using OpenSSL_jll
 JLLWrappers.@generate_wrapper_header("Python")
-JLLWrappers.@declare_library_product(libpython, "@rpath/libpython3.8.dylib")
+JLLWrappers.@declare_library_product(libpython, "@rpath/libpython3.10.dylib")
 JLLWrappers.@declare_executable_product(python)
 function __init__()
-    JLLWrappers.@generate_init_header(Expat_jll, Bzip2_jll, Libffi_jll, Zlib_jll, XZ_jll, OpenSSL_jll)
+    JLLWrappers.@generate_init_header(Expat_jll, Bzip2_jll, Libffi_jll, LibMPDec_jll, Zlib_jll, XZ_jll, OpenSSL_jll)
     JLLWrappers.@init_library_product(
         libpython,
-        "lib/libpython3.8.dylib",
+        "lib/libpython3.10.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
